b5efd1a94072f77466d8d6ac419121df18ead1c5
Tauno Erik <sedumacre@gmail.com>
2025-07-22 15:22:44 +0300

    loogika

--- Kood/Etteande-automaat/src/main.cpp
+++ Kood/Etteande-automaat/src/main.cpp
@@ -2,7 +2,7 @@
  * Projekt:  Etteande automatiseerimine
  * Autor:    Tauno Erik
  * Algus:    2025.06.26
- * Muudetud: 2025.06.27
+ * Muudetud: 2025.07.20
  */
 #include <Arduino.h>
 
@@ -11,25 +11,28 @@
  Samm-mootorid
 **************************************************/
 // Mootori suund
-#define CW  0  // clockwise
-#define CCW 1  // counterclockwise
+#define CW      0  // clockwise
+#define FORWARD 0
+#define CCW     1  // counterclockwise
+#define BACK    1
 // Motor 1 pins
-const int M1_PULSE_PIN = 2;
-const int M1_DIRECTION_PIN = 3;
+const int M1_PULSE_PIN = 2;      // D2
+const int M1_DIRECTION_PIN = 3;  // D3
 // Speed of the motor
 const int M1_SPEED = 500;
 // Function prototypes
-void run_steps(int dir, int steps, int speed, int pulse_pin, int direction_pin);
-void run(int speed, int pulse_pin);
 void init_motor();
+void run_step_motor(int dir, int steps, int speed, int pulse_pin, int direction_pin);
+//void run(int speed, int pulse_pin);
+
 
 
 /*************************************************
  Lülitid
 **************************************************/
 // Pins
-const int LEFT_BTN_PIN = 7;
-const int RIGHT_BTN_PIN = 8;
+const int LEFT_BTN_PIN = 7;   // D7
+const int RIGHT_BTN_PIN = 8;  // D8
 // Timing
 const unsigned long RELEASE_WINDOW = 200;  // milliseconds
 unsigned long left_release_time = 0;
@@ -41,25 +44,30 @@ void init_switches(int left_pin, int right_pin);
  Relee
 **************************************************/
 // Pins
-const int RELEE_PIN = 5;
+const int RELEE_PIN = 9;  // D9
 // Function prototypes
 void init_relee();
 void relee_ON();
 void relee_OFF();
 
 /*************************************************
+ Küsi robotilt
+**************************************************/
+void ask_from_robot();
+
+/*************************************************
 // Olekud
 **************************************************/
 enum State {
-  IDLE,             // Tühikäik
-  WAIT_NEW_DETAILS,
+  IDLE,             // Ootab
   ASK_NEW_DETAILS,  // Ütle robotile
-  MOVE_FORWARD,     // Mootorid
-  PUSH,             //Relee
-  ERROR
+  MOVE_FORWARD,     // Mootorid liiguvad
+  PUSH,             // Relee lülitatud
+  ERROR             // Viga
 };
 
-State current_state = WAIT_NEW_DETAILS;
+// Init state
+State current_state = IDLE;
 
 
 
@@ -76,35 +84,34 @@ void setup() {
 
 void loop() {
   // Lülitid ////////////////////////////////////////////////
-  static bool last_left_state = LOW;
-  static bool last_right_state = LOW;
+  static bool last_left_btn_state = LOW;
+  static bool last_right_btn_state = LOW;
 
-  bool left_state = digitalRead(LEFT_BTN_PIN);
-  bool right_state = digitalRead(RIGHT_BTN_PIN);
+  bool left_btn_state = digitalRead(LEFT_BTN_PIN);
+  bool right_btn_state = digitalRead(RIGHT_BTN_PIN);
 
   // Detect left button release
-  if (last_left_state == HIGH && left_state == LOW) {
+  if (last_left_btn_state == HIGH && left_btn_state == LOW) {
     left_release_time = millis();
-    Serial.println("Left button released");
+    Serial.println("Vasak nupp vabastatud");
   }
 
   // Detect right button release
-  if (last_right_state == HIGH && right_state == LOW) {
+  if (last_right_btn_state == HIGH && right_btn_state == LOW) {
     right_release_time = millis();
-    Serial.println("Right button released");
+    Serial.println("Parem nupp vabastatud");
   }
 
-  last_left_state = left_state;
-  last_right_state = right_state;
+  last_left_btn_state = left_btn_state;
+  last_right_btn_state = right_btn_state;
 
   // Check if both buttons were released within the allowed window
   if (left_release_time != 0 && right_release_time != 0 &&
-      abs((long)(left_release_time - right_release_time)) <= RELEASE_WINDOW) {
-    
-    // Turn LED on for 1 second
-    digitalWrite(LED_PIN, HIGH);
-    delay(1500);
-    digitalWrite(LED_PIN, LOW);
+      abs((unsigned long)(left_release_time - right_release_time)) <= RELEASE_WINDOW) {
+      Serial.println("Mölemad nupud vabastatud");
+    // Detaili ei ole
+    // Küsi robotilt
+    current_state = ASK_NEW_DETAILS;
 
     // Reset release times to prevent retriggering
     left_release_time = 0;
@@ -115,17 +122,45 @@ void loop() {
   switch (current_state) {
     case IDLE:
       // Oota
-      Serial.println("State: IDLE");
-      
-      if (digitalRead(2) == HIGH) {
-        currentState = RUNNING;
-      }
+      Serial.println("switch: state: IDLE");
+      delay(1000);
+      // Järgmine samm:
+      current_state = MOVE_FORWARD;
+      break;
 
+    case ASK_NEW_DETAILS:
+      Serial.println("switch: state: ASK_NEW_DETAILS");
+      // TODO:
+      ask_from_robot();
+      delay(1000);
+      // Järgmine samm:
+      current_state = MOVE_FORWARD;
+      break;
+
+    case MOVE_FORWARD:
+      Serial.println("switch: state: MOVE_FORWARD");
+      // Liiguta edasi mootoreid
+      run_step_motor(FORWARD, 500, M1_SPEED, M1_PULSE_PIN, M1_DIRECTION_PIN);
+      //delay(10);
+      // Liiguta mootoreid nõks tagasi
+      run_step_motor(BACK, 500, M1_SPEED, M1_PULSE_PIN, M1_DIRECTION_PIN);
+      //delay(10);
+      // Järgmine samm:
+      current_state = PUSH;
+      break;
+
+    case PUSH:
+      Serial.println("switch: state: PUSH");
+      relee_ON();
+      delay(1000);
+      relee_OFF();
+      // Järgmine samm:
+      current_state = MOVE_FORWARD;
       break;
 
     case ERROR:
       // Viga
-      Serial.println("State: Error!");
+      Serial.println("switch: state: ERROR");
       current_state = IDLE; // Näiteks muudame tagasi ootama
       break;
   }
@@ -139,7 +174,7 @@ void loop() {
  * @param speed Speed of the motor
  **********************************************************************/
 
-void run_steps(int dir, int steps, int speed, int pulse_pin, int direction_pin)
+void run_step_motor(int dir, int steps, int speed, int pulse_pin, int direction_pin)
 {
   digitalWrite(direction_pin, dir);  // Set direction
 
@@ -157,6 +192,7 @@ void run_steps(int dir, int steps, int speed, int pulse_pin, int direction_pin)
  * Run the stepper motor forever until both buttons are unpressed
  * @param speed Speed of the motor
  **********************************************************************/
+ /*
 void run(int speed, int pulse_pin)
 {
   while (true)
@@ -173,6 +209,7 @@ void run(int speed, int pulse_pin)
     }
   }
 }
+*/
 
 void init_switches(int left_pin, int right_pin) {
   pinMode(left_pin, INPUT);   // Pulled down externally
@@ -200,4 +237,8 @@ void relee_ON() {
 void relee_OFF() {
   digitalWrite(RELEE_PIN, LOW);
   Serial.println("Relee OFF");
+}
+
+void ask_from_robot() {
+  println("Küsin ROBERTALT");
 }
